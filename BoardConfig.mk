49fe8255a7019e5bdc672ae417ba2de457db1c7e
Skena <ajansyidad2014@gmail.com>
2025-11-25 13:10:24 +0700

    alioth: Include firmware configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,5 +24,8 @@ TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Firmware
+include vendor/xiaomi/alioth-firmware/BoardConfigVendor.mk
+
 # Inherit from the proprietary version
 include vendor/xiaomi/alioth/BoardConfigVendor.mk
