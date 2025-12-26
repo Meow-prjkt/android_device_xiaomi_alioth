ba7b375b76f2c2555e31fb2773fc34da6b32b70a
skenakun <ajansyidad2014@gmail.com>
2025-12-26 17:16:16 +0000

    alioth: Welcome to yaap AOSP

--- yaap_alioth.mk
+++ yaap_alioth.mk
@@ -9,14 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
-
-# Inherit Camera-related flags
-TARGET_USES_MIUI_CAMERA := true
-TARGET_INCLUDES_MIUI_CAMERA := true
 
 # Enable UI enhancements
 TARGET_ENABLE_BLUR := true
@@ -27,8 +23,11 @@ BYPASS_CHARGE_SUPPORTED := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 USE_PIXEL_CHARGING := true
 
+# Flags
+TARGET_BUILD_GAPPS := true
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := yaap_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
